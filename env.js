e671570efee9ff1cbf8510a1cf6f0a5240fca92f
Tushar-Samodrei <tushar.khurana@samodrei.com>
2026-05-01 12:11:38 +0530

    Added env.js

---
+++ env.js
@@ -0,0 +1,5 @@
+window.env = {
+  API_BASE_URL: "https://ai.samodrei.com/emailapi",
+  RAG_API_BASE: "https://ai.samodrei.com/earagapi",
+  RAG_META_BASE: "https://ai.samodrei.com/earagmetaapi",
+};
